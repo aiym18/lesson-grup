5e0953a05093c1edb2123a64dc32cffa115aceb3
CholponaiNaimanova <161929973+CholponaiNaimanova@users.noreply.github.com>
2024-05-18 14:17:58 +0600

    Update index.js

--- src/companets/Header/index.js
+++ src/companets/Header/index.js
@@ -1,43 +1,61 @@
-import React from "react";
-import headerimg from "../../img/headerimg.png";
-import { BsGeoAlt } from "react-icons/bs";
-import { BsTelephone } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import React from 'react';
+import senter from '../../assets/img/senter.png'
+import image1 from '../../assets/img/image1.png'
+import image2 from '../../assets/img/image2.png'
+import { IoLocationOutline } from "react-icons/io5";
+import { LuPhone } from "react-icons/lu";
+
 
 const Header = () => {
-  return (
-    <div id="header">
-      <div className="continer">
-        <div className="header">
-          <div className="header--start">
-            <img src={headerimg} alt="" />
-            <div className="header--start__nav">
-              <a>
-                <BsGeoAlt />
-                Маршала Катукова, д.8
-              </a>
-              <a>
-                <BsTelephone />
-                +7 (495) 750 05 00
-              </a>
+    return (
+        <div id="header">
+            <div className="container">
+                <div className="header">
+
+                    <div className="headerone">
+                        
+                    <img src={senter} alt="" />
+
+<div className="header-address">
+    <a>
+    <IoLocationOutline />
+    </a>
+    <p>Маршала Катукова, д.8</p>
+
+    <a>
+    <LuPhone />
+    </a>
+    <p>+7 (495) 750 05 00</p>
+</div>
+
+<button>Заказать звонок</button>
+                    </div>
+
+                    <div class="header-nav">
+
+                    <a href="#">О центре</a>
+                    <a href="#">Афиша</a>
+                    <a href="#">Студии</a>
+                    <a href="#">Расписание</a>
+                    <a href="#">Пространства</a>
+                    <a href="#">Абонементы</a>
+                    <a href="#">Организаторам</a>
+                    <a href="#">Контакты</a>
+
+                </div>
+
+                <div class="image">
+                    <img src={image1} alt="" width="100%"/>
+
+                    <div class="image--two">
+                        <img src={image2} alt=""/>
+                    </div>
+                    <h1>Движение. Творчество. Рост.</h1>
+                </div>
+                </div>
             </div>
-            <button className="btn">Заказать звонок</button>
-          </div>
-          
         </div>
-      </div>
-      <div className="and">
-          <Link to={""}>О центре</Link>
-          <Link to={""}>Афиша</Link>
-          <Link to={""}>Студии</Link>
-          <Link to={""}>Расписание</Link>
-            <Link to={""}>Пространства</Link>
-            <Link to={""}>Абонементы</Link>
-            <Link to={""}>Организаторам</Link>
-            <Link to={""}>Контакты</Link>
-          </div>
-    </div>
-  );
+    );
 };
 
 export default Header;
